2ca9b7f8a2d42cc534e4ff9bccccdd4feb4e8f3d
i-s-u-r-u <98538395+i-s-u-r-u@users.noreply.github.com>
2023-06-09 17:31:18 +0000

    added jobs

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+jobs = [{
+  "id": 1,
+  "title": "Data Analyst",
+  "location": "Galle",
+  "salary": "Rs. 100,000"
+}, {
+  "id": 2,
+  "title": "Data Scienetist",
+  "location": "Colombp",
+  "salary": "Rs. 200,000"
+}, {
+  "id": 1,
+  "title": "Frontend Engineer",
+  "location": "remote",
+  "salary": "Rs. 100,000"
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=jobs)
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(jobs)
 
 
 if __name__ == '__main__':
